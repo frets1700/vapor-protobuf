9df40112a8c56624bec5a59ec4dc2466b85a5543
frets1700 <panmol1700@gmail.com>
2020-10-14 16:58:04 -0400

    adding initial package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "vapor-protobuf",
+    products: [
+        .library(
+            name: "VaporProtocolBuffers",
+            targets: ["VaporProtocolBuffers"]),
+    ],
+    dependencies: [
+//		.package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0")
+    ],
+    targets: [
+        .target(
+            name: "VaporProtocolBuffers",
+            dependencies: []),
+        .testTarget(
+            name: "VaporProtocolBuffersTests",
+            dependencies: ["VaporProtocolBuffers"]),
+    ]
+)
